e2e0c06efab291e229506ec8c17cd1bb1230f3e8
shahidulhasan <er.shahidul@gmail.com>
2017-02-07 17:25:52 +0600

    work in cart page

--- application/views/cart/index_view.php
+++ application/views/cart/index_view.php
@@ -104,17 +104,53 @@
 					</tr>
 					</thead>
 					<tbody>
-					<tr>
-						<td> <img width="60" src="themes/images/products/4.jpg" alt=""/></td>
-						<td>MASSA AST<br/>Color : black, Material : metal</td>
-						<td>
-							<div class="input-append"><input class="span1" style="max-width:34px" placeholder="1" id="appendedInputButtons" size="16" type="text"><button class="btn" type="button"><i class="icon-minus"></i></button><button class="btn" type="button"><i class="icon-plus"></i></button><button class="btn btn-danger" type="button"><i class="icon-remove icon-white"></i></button>				</div>
-						</td>
-						<td>$120.00</td>
-						<td>$25.00</td>
-						<td>$15.00</td>
-						<td>$110.00</td>
-					</tr>
+
+<?php if( count( $cart ) > 0 )
+{
+    $qry = $total = $i = 0;
+    foreach( $cart as $detail ) {
+        $row = $product[$detail['id']];
+        $i++;
+        $total += $price = number_format( $row->product_price * $detail['qty'], 2, '.', '' );
+        $qry += $detail['qty'];
+        echo "
+			<tr class='" . ( $i % 2 == 0 ? "even" : "odd" ) . "_row'>
+				<td>$i</td>
+				<td>
+				    <img src='" . base_url() ."assets/product/" . ( $row->phy_path == "" ? "nopicture.png" : $row->phy_path ) . "' title='$row->product_name' width='60' />
+				</td>
+				<td>
+				    <a href='" . base_url() . "home/product/$row->id'>$row->product_name</a><br/>
+				    
+				</td>
+				<td>
+					<a href='" . base_url() . "home/content/category/$row->id_category'>" . ( $category[$row->id_category] ) . "</a> 
+					-
+					<a href='" . base_url() . "home/content/subcategory/$row->id_subcategory/'>" . ( $subcategory[$row->id_subcategory] ) . "</a>
+				</td>
+				<td>$ $row->product_price</td>
+				<td>
+					<a href='" . base_url() . "cart/remove_item/$row->id/1' title='Remove One'><img src='" . base_url() . "assets/images/quantity_down.gif' /></a>
+					$detail[qty]
+					<a href='" . base_url() . "cart/add_item/$row->id' title='Add One'><img src='" . base_url() . "assets/images/quantity_up.gif' /></a>
+				</td>
+				<td>$ $price <a href='" . base_url() . "cart/remove_item/$row->id/all' title='Remove This'><img src='" . base_url() . "assets/images/cart_cross_small.png' /></a></td>
+			</tr>
+					";
+    } } ?>
+
+
+<!--					<tr>-->
+<!--						<td> <img width="60" src="themes/images/products/4.jpg" alt=""/></td>-->
+<!--						<td>MASSA AST<br/>Color : black, Material : metal</td>-->
+<!--						<td>-->
+<!--							<div class="input-append"><input class="span1" style="max-width:34px" placeholder="1" id="appendedInputButtons" size="16" type="text"><button class="btn" type="button"><i class="icon-minus"></i></button><button class="btn" type="button"><i class="icon-plus"></i></button><button class="btn btn-danger" type="button"><i class="icon-remove icon-white"></i></button>				</div>-->
+<!--						</td>-->
+<!--						<td>$120.00</td>-->
+<!--						<td>$25.00</td>-->
+<!--						<td>$15.00</td>-->
+<!--						<td>$110.00</td>-->
+<!--					</tr>-->
 
 					<tr>
 						<td colspan="6" style="text-align:right">Total Price:	</td>
